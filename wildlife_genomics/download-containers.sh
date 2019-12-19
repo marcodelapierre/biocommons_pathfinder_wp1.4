be1f07d357a7e172da46badccbc1f0292e9f163e
Marco De La Pierre <marco.delapierre@gmail.com>
2019-12-19 11:53:49 +0800

    updated trinity container version

--- wildlife_genomics/download-containers.sh
+++ wildlife_genomics/download-containers.sh
@@ -20,7 +20,7 @@ quay.io/biocontainers/busco:3.0.2--py37_12
 quay.io/biocontainers/rsem:1.3.2--pl526r351hc0aa232_0
 quay.io/biocontainers/trinotate:3.2.0--pl526_0
 quay.io/biocontainers/maker:2.31.10--pl526_14
-quay.io/biocontainers/trinity:2.8.4--py36pl526hff88cb2_1
+quay.io/biocontainers/trinity:2.8.5--h8b12597_5
 marcodelapierre/supernova:2.1.1
 marcodelapierre/juicer:27Aug19-cpu
 "
